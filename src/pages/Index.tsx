91c116212658346c39386e48c99cab345ac181f4
cpa672 <cpa672@gmail.com>
2025-10-26 22:20:56 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
         {/* Logo Area - White Background */}
         <div className="bg-background py-2 md:py-3">
           <div className="container mx-auto px-4 md:px-6 text-center animate-fade-in">
-            <div className="max-w-md mx-auto">
+            <div className="max-w-sm mx-auto">
               <img 
                 src={logo} 
                 alt="KTML Holdings - E-commerce & Retail Partnership" 
